3eb82edd7884bae9fab09a2efc818b4ba5fc7c74
Muhammad Musa <musaghauri@hotmail.com>
2020-09-22 17:56:41 +0500

    chore: Changed getAll promise to async + await

--- src/App.js
+++ src/App.js
@@ -17,10 +17,9 @@ class BooksApp extends React.Component {
     books: [],
   }
 
-  componentDidMount() {
-    BooksAPI.getAll().then(books => {
-      this.setState({ books })
-    });
+  async componentDidMount() {
+    const books = await BooksAPI.getAll()
+    this.setState({ books })
   }
 
   handleCategoryChange = (e, id) => {
